ee6379782934d427e23e6379b1a03b7063e7ce4b
Lauren Mastroni <laurenmastroni@gmail.com>
2017-12-17 18:14:03 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <!-- the router outlet, where all matched components would ber viewed -->
+    <div class="links">
+      <router-link v-bind:to="'/'">Home</router-link>
+      <router-link v-bind:to="'/about'">About</router-link>
+      <router-link v-bind:to="'/contact'">Contact</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+<!-- styling for the component -->
+<style>
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+}
+
+.links {
+  /*z-index: 1;*/
+  text-align: right;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+a:visited {
+  color: #fff;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <h5>{{ tagline }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome',
+      tagline: 'Lorem ipsum dolor sit amet'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  margin-top: -85px;
+  background-image: url('../assets/background.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  /*z-index: 2;*/
+}
+
+h1 {
+  text-transform: uppercase;
+  font-size: 6em;
+  letter-spacing: 5px;
+  padding-top: 150px;
+}
+
+h5 {
+  font-weight: normal;
+  font-size: 1.3em;
+  letter-spacing: 10px;
+  margin-top: -90px;
+}
+
+
+</style>
